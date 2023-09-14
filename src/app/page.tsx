dd2186893b62e125ecadda2345f3dde0025e28b6
uandersonmbc@gmail.com <uandersonmbc@gmail.com>
2023-09-14 17:49:56 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Input from '@/components/atoms/Input';
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="w-80">
+        <Input placeholder="teste" />
+      </div>
+    </main>
+  );
+}
